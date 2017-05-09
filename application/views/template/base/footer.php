275b3e39ed8370eaa926e308dfc3af34c9e0e653
MWASO Jordan <mwaso.jordan@hotmail.com>
2017-05-09 16:30:37 +0200

    Intégration de font-awesome

---
+++ application/views/template/base/footer.php
@@ -0,0 +1,17 @@
+
+
+
+<div class="row" style="background-color: #001a2c; margin-top: 10px; height: 35px;  "> <!--MY FOOTER-->
+        <div class="col-md-3">
+            <p>Font Awesome</p>
+        </div>
+        <div class="col-md-3">
+            <p>Legal terms</p>
+        </div>
+</div>
+
+
+
+<!--<p>Privacy police</p>
+<p>Legal terms</p>
+<p>Credits</p>-->
---
+++ application/views/template/base/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.css')?>">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+    $this->load->view('template/base/footer');
+
+?>
+
+
+</body>
+</html>
