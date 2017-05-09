487b6cb46bfb490290b181b3a03c7da3653c0043
MWASO Jordan <mwaso.jordan@hotmail.com>
2017-05-09 15:33:26 +0200

    footer version beta + modification de la config

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap.css') ?>">
 	<title>Welcome to CodeIgniter</title>
 
 	<style type="text/css">
@@ -84,6 +85,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
-
+<?php $this->load->view('template/base/footer'); ?>
 </body>
 </html>
