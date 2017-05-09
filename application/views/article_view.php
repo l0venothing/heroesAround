ef950967ca673f81e7f32679f744b2bf28c3e194
2fiq Developper <eltowfiq@gmail.com>
2017-05-09 13:25:45 +0200

    ma branch installation du css

---
+++ application/views/article_view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("../../assets/css/style.css")?>">
+    <title>Heroes Around</title>
+</head>
+<body>
+    <div class="row">
+
+<div class="col-md-6"> <img src="" alt=""></div>
+
+<div class="col-md-6"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit facilis repudiandae architecto aspernatur maiores porro saepe sit nostrum suscipit officia expedita ullam, minima assumenda totam recusandae perspiciatis laborum nulla iusto?</p></div>
+
+</div>
+
+<div class="row">
+
+    <div class="col-md-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, adipisci, impedit! Quidem iusto, officiis error nihil. Alias aliquam dolores laudantium tenetur sunt eaque obcaecati vero, nihil sint ducimus iure, mollitia!</div>
+
+
+    <div class="col-md-6"><img src="" alt=""></div>
+
+</div>
+</body>
+</html>
