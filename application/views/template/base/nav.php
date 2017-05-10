81e324f053269928e73039b6f736c2853c3a7e9c
amelia <yu.a@live.com>
2017-05-10 16:22:26 +0200

    creation nav.php2

---
+++ application/views/template/base/nav.php
@@ -0,0 +1,22 @@
+ <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">HEROES AROUND</a>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?=base_url('');?>">Home</a></li>
+                <li><a href="<?=base_url('');?>">Why us?</a></li>
+                <li><a href="<?=base_url('');?>">Download</a></li>
+                <li><a href="<?=base_url('');?>">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
