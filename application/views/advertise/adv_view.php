8b013076826dee393d83b6c52b11381af7df5610
Esther Roux <esther.roux13@gmail.com>
2017-05-10 10:16:11 +0200

    ajout de .haccess Assets Advertise download app controller et view

---
+++ application/views/advertise/adv_view.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="">
+    <title>Heroes Around</title>
+    <link rel="stylesheet" href="http://heroesaround.dev/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://heroesaround.dev/assets/css/style-adv.css">
+
+</head>
+<body>
+<div class="container">
+    <div class="row text-center">
+        <div class="col-xs-12"><h3>Télécharge l'appli des héros ! </h3></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-4">
+            <div id="img-sm">
+            <img id="sm" src="http://heroesaround.dev/assets/img/smartphone.png" alt="">
+            </div>
+        </div>
+        <div class="col-xs-6 download-app-text">
+        <div class="">
+        <p>Un héros porte toujours Heroes Around avec lui ! </p> 
+        <p>Rendez-vous sur App Store ou Google Play et télécharge vite Heroes Around !</p>
+        </div>
+        </div>
+    </div>
+    <div class="row">
+       <div class="col-xs-12 text-center">
+       <a href="#" class="button"><img class="download-app" src="http://heroesaround.dev/assets/img/available-app-store.png" alt=""></a>
+       <a href="#" class="button"><img class="download-app" src="http://heroesaround.dev/assets/img/available-g-play.png" alt=""></a></div>
+    </div>
+</div>
+
+ <script src="http://heroesaround.dev/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="http://heroesaround.dev/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
