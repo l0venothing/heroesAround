bbca7dd8e31d12901d0e233ae20c674c7d0cc445
Esther Roux <esther.roux13@gmail.com>
2017-05-10 15:18:05 +0200

    correction virtualhosts >  base_url

--- application/views/template/base/index.php
+++ application/views/template/base/index.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
 
-    <meta name="viewport" content="width=², initial-scale=1.0">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
  
  <!-- Latest compiled and minified CSS & JS -->
  <!--<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
  <script src="//code.jquery.com/jquery.js"></script>
  <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>-->
-  <link rel="stylesheet" href="http://heroesaround.dev/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://heroesaround.dev/node_modules/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="http://heroesaround.dev/assets/css/style-adv.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url("../../assets/css/style.css")?>">
+  <link rel="stylesheet" href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css');?>">
+    <link rel="stylesheet" href="<?= base_url('node_modules/font-awesome/css/font-awesome.min.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style-adv.css');?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/style.css");?>">
     <title>Document</title>
 </head>
 <body>
