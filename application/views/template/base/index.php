8451d4943641699a70b827d946253b6b7cee6c7a
Esther Roux <esther.roux13@gmail.com>
2017-05-10 15:06:32 +0200

    install font awesome, fin du rassemblement des vues > why us / advertise / footer

--- application/views/template/base/index.php
+++ application/views/template/base/index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
 
-<<<<<<< HEAD
     <meta name="viewport" content="width=², initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
  
@@ -12,6 +11,7 @@
  <script src="//code.jquery.com/jquery.js"></script>
  <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>-->
   <link rel="stylesheet" href="http://heroesaround.dev/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://heroesaround.dev/node_modules/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="http://heroesaround.dev/assets/css/style-adv.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url("../../assets/css/style.css")?>">
     <title>Document</title>
