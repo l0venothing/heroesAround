b757ba07e92089d0e2417cdb20e797d8638592e5
Esther Roux <esther.roux13@gmail.com>
2017-05-10 14:32:45 +0200

    transfert footer vers dev

--- application/views/template/base/footer.php
+++ application/views/template/base/footer.php
@@ -18,6 +18,7 @@
                         <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
                         <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                         <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        
                     </div>
                     <!--<div class="row">
                         <i class="fa fa-dropbox" aria-hidden="true"></i>
