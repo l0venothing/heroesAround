f420e2c273582d989ac761478d41a58e6a4834dd
amelia <yu.a@live.com>
2017-05-12 01:35:58 +0200

    modif code taufiq + indentation

--- application/views/article_view.php
+++ application/views/article_view.php
@@ -31,27 +31,38 @@
 
     </header> -->
 
-<div class="row whyus">
-<h2> Why Us? </h2>
+<div class="row whyus" id="heroes">
+    <h2> Why Us? </h2>
 </div>
 
-    <div class="row partieun">
+    <div class="container">
+<div class="row partieun">
 
-<div class="col-md-6"> <img id="imgaccueil" src="assets/img/info.jpg" alt="heroes info"></div>
+    <div class="col-md-6">
+        <img id="imgaccueil" src="assets/img/info.jpg" alt="heroes info">
+    </div>
 
-<div class="col-md-6"><p>Notre équipe travaille sans cesse sur le développement du système de géolocalisation
-et de GPS pour accélérer le temps de localisation de votre téléphone et également simplifier
-les manipulations d'appels d'urgence dans un temps record.</p></div>
+    <div class="col-md-6">
+        <p class="para">Notre équipe travaille sans cesse sur le développement du système de géolocalisation
+        et de GPS pour accélérer le temps de localisation de votre téléphone et également simplifier
+        les manipulations d'appels d'urgence dans un temps record.
+        </p>
+    </div>
 
 </div>
 
 <div class="row partieun">
 
-    <div class="col-md-6"><p>Nous travaillons d'autre part, en partenariat avec les services de secours, sur le temps d'arrivée des services de secours qui est estimé pour le moment à 9 minutes et quelques secondes. De plus, il y a un nombre important de médecins et pompiers s'élevant jusqu'à 80.000 professionnels.</p></div>
+    <div class="col-md-6">
+        <p class="para">Nous travaillons d'autre part, en partenariat avec les services de secours, sur le temps d'arrivée des services de secours qui est estimé pour le moment à 9 minutes et quelques secondes. De plus, il y a un nombre important de médecins et pompiers s'élevant jusqu'à 80.000 professionnels.
+        </p>
+    </div>
 
 
     <div class="col-md-6">
-        <img id="imgaccueil" src="assets/img/brussels-victim.jpg" alt="heroes"></div>
+        <img id="imgaccueil" src="assets/img/brussels-victim.jpg" alt="heroes">
+    </div>
 
+</div>
 </div>
 </div>
